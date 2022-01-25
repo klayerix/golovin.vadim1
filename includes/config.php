805fd3a06919b38f80e9dd3305d07175911755d8
klayerix <52912977+klayerix@users.noreply.github.com>
2022-01-25 17:55:13 +0300

    Add files via upload
    upload project

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<head>
+ <link rel="shortcut icon" href="../static/images/favicon.ico" type="image/x-icon">
+</head>
+<?php
+$config = array(
+    'title' => 'Студенческий портал ФКН',
+    'url' => 'http://f0451479.xsph.ru/task.html',
+    'db' => array(
+        'server' => 'localhost',
+        'username' => 'admin',
+        'password' => '123',
+        'name' => 'admin'
+
+    )
+);
+require "db.php";
